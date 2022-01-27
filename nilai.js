514ce580f9a58f9f399b73c1e83459dfb0361478
wiandamumtaz <wianda_mumtaz_29rpl@student.smktelkom-mlg.sch.id>
2022-01-27 07:53:11 +0700

    "assignmentAPI"

--- nilai.js
+++ nilai.js
@@ -1,17 +1,48 @@
-// load library express
-const { request, response } = require("express")
-let express = require("express")
+const express = require ("express")
 
-//inisiasi objek baru dari express
-let app = express()
+//inisialisasi
+const app = express()
 
+// set to read a request data from JSON format
 app.use(express.json())
-//end point untuk supermarket
-app.post("/pecahan", (request,response) => {
-    let 
-    
+// JSON is String
+
+app.post("/nilai",(request,response)=>{
+    let data = request.body.school
+    let rata = 0, rataSiswa = 0
+    let lulus = []
+    let tidak_lulus = []
+
+    // for untuk emnghitung rata keseluruhan
+    for (let i = 0; i < data.length; i++) {
+        rata += data[i].math+data[i].english
+    }
+    rata = rata/8
+
+    //for untuk menghitung rata@ per siswa
+    for (let j = 0; j < data.length; j++) {
+
+        rataSiswa = (data[j].math+data[j].english)/2
+
+        if (rataSiswa>=rata) {
+            lulus.push({
+                nama : data[j].nama
+            })
+        } else if(rataSiswa<rata){
+            tidak_lulus.push({
+                nama : data[j].nama
+            })
+        }
+        
+    }
+    return response.json({
+        lulus,
+        tidak_lulus,
+        Rata_rata : `${rata}`
+    })
 })
 
-app.listen(8001,() => {
-    console.log(`server run on post 8001`);
+
+app.listen(8000, () => {
+    console.log(`Server run on port 8000`);
 })
